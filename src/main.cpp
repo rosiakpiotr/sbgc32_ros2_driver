14bed87e12a2a201bad837597912457304c8a7e3
pi <raspberry@none>
2022-11-20 13:18:43 +0000

    Gimbal follows circular pattern (control works!)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
-
+#include "gimbal.hpp"
+#include <math.h>
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Gimbal gimbal;
+    gimbal.initializeDriver();
+    gimbal.configControl();
+    gimbal.motorsOn();
+
+    const int R = 25;
+    const int resolution = 720;
+    double step = (360.0 / (double)resolution) * M_PI / 180.0;
+
+    while(true)
+    {
+        FOR_(i, resolution)
+        {
+            gimbal.movePitchTo(R * sin(i*step));
+            gimbal.moveYawTo(R * cos(i*step));
+            usleep(10000);
+        }
+    }
+
+    gimbal.motorsOff();
     return 0;
 }
-
